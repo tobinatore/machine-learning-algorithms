420d8d3ada681fc0367d82789cffa7e7384fc902
Tobias <germanicalp@gmail.com>
2019-07-24 22:09:34 +0200

    Added Linear Regression and KNN
    Added source files and datasets for Linear Regression and
    K-Nearest-Neighbour.

---
+++ KNN/knn.py
@@ -0,0 +1,79 @@
+import sklearn
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import preprocessing
+import pandas as pd
+import pickle
+
+# --------------------------------------------------------------------------------------------------------- #
+# A script using K-Nearest-Neighbour-Algorithm to classify a vehicle based on buying and maintenance price, #
+# no. of doors, capacity in terms of persons, size of luggage boot and the car's safety rating.             #
+#                                                                                                           #
+#                            https://archive.ics.uci.edu/ml/datasets/Car+Evaluation                         #
+# --------------------------------------------------------------------------------------------------------- #
+
+def show_output(predictions, x_test, y_test, model):
+    """
+    A function for printing the output our algorithm generates.
+    :param predictions: List[]
+    :param x_test: List[]
+    :param y_test: List[]
+    :param model: KNeighboursClassifier()
+    :return: None
+    """
+    for x in range(len(predictions)):
+        print("Prediction: ", predictions[x])
+        print("Input data: ", x_test[x])
+        print("Actual Class: ", y_test[x])
+        print("----")
+
+    print("Total Accuracy:", model.score(x_test, y_test), "equals ", round(model.score(x_test, y_test) * 100, 2),
+          "% correctly estimated.")
+
+
+# Read car.data like a CSV - as all values are comma separated
+# I added a header row to the original dataset so it can be
+# read in directly using pandas.
+data = pd.read_csv("car.data")
+
+# Using the sklearn preprocessor, we're transforming non-integer values to integers
+# so we can train our model.
+preprocessor = preprocessing.LabelEncoder()
+buying = preprocessor.fit_transform(list(data["buying"]))
+maintenance = preprocessor.fit_transform(list(data["maintenance"]))
+doors = preprocessor.fit_transform(list(data["doors"]))
+persons = preprocessor.fit_transform(list(data["persons"]))
+lug_boot = preprocessor.fit_transform(list(data["lug_boot"]))
+safety = preprocessor.fit_transform(list(data["safety"]))
+cls = preprocessor.fit_transform(list(data["class"]))
+
+predict = "class"
+
+x = list(zip(buying, maintenance, doors, persons, lug_boot, safety))
+y = list(cls)
+
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
+
+acc = 0.0
+# Same as in the script for Linear Regression.
+# We're trying to load a pickled model, if there is none,
+# train our model until it has an accuracy greater than 97%
+# then save it for later use.
+try:
+    pickle_file = open("knn.pickle", "rb")
+except:
+    while acc < 0.97:
+        x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
+        model = KNeighborsClassifier(n_neighbors=7)
+        model.fit(x_train, y_train)
+        acc = model.score(x_test, y_test)
+
+    with open("knn.pickle", "wb") as file:
+        pickle.dump(model, file)
+
+    pickle_file = open("knn.pickle", "rb")
+
+model = pickle.load(pickle_file)
+predictions = model.predict(x_test)
+
+show_output(predictions, x_test, y_test, model)
+
---
+++ Linear_Regression/linear_regression.py
@@ -0,0 +1,100 @@
+import pandas as pd
+import numpy as np
+import sklearn
+from sklearn import linear_model
+import matplotlib.pyplot as pyplot
+from matplotlib import style
+import pickle
+
+# -------------------------------------------------------------------------------------------------------- #
+# A script using linear regression to estimate the grades of students in G3 based on their results in G1   #
+# and G2 as well as their absences during the academic year, their failures and the time studied per week. #
+#                                                                                                          #
+#                                This script uses the following dataset:                                   #
+#                       https://archive.ics.uci.edu/ml/datasets/Student+Performance                        #
+# -------------------------------------------------------------------------------------------------------- #
+
+
+def plot_scatter_diagram(data):
+    """
+    Plots a chosen relation in a scatter plot.
+    :param data: DataFrame
+    :return: None
+    """
+    att = "G1"
+    style.use("ggplot")
+    pyplot.scatter(data[att], data["G3"])
+    pyplot.xlabel(att)
+    pyplot.ylabel("Final Grade")
+    pyplot.show()
+
+
+def show_output(predictions, x_test, y_test, linear):
+    """
+    A function for printing the output our algorithm generates.
+    :param predictions: List[]
+    :param x_test: List[]
+    :param y_test: List[]
+    :param linear: LinearRegression()
+    :return: None
+    """
+    for x in range(len(predictions)):
+        print("Prediction: ", predictions[x])
+        print("Input data: ", x_test[x])
+        print("Actual Final Grade: ", y_test[x])
+        print("----")
+
+    print("Total Accuracy:", linear.score(x_test, y_test), "equals ", round(linear.score(x_test, y_test) * 100, 2),
+          "% correctly estimated.")
+
+
+def read_data(filename):
+    """
+    Function for reading the CSV-file and dropping all columns that aren't important for our purpose.
+    :param filename: String
+    :return: DataFrame
+    """
+    dat = pd.read_csv(filename, sep=";")
+    dat = dat[["G1", "G2", "G3", "studytime", "failures", "absences"]]
+    return dat
+
+
+data = read_data("student-mat.csv")
+predict = "G3" # specifying which attribute we want to predict
+
+# Our x and y are going to be the whole DataFrame without the G3 column
+# and only the G3 column respectively
+x = np.array(data.drop([predict], 1))
+y = np.array(data[predict])
+
+# Splitting our dataset in different training and testing sets
+# 90% of the data will be used for training, 10% for testing
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
+
+acc = 0.0
+
+# Here we'll get our saved model,
+# or if we're doing the computation the first time (in which case open() will throw a FileNotFound-Exception)
+# we'll save a model with an accuracy > 95% using pickle.
+# Albeit not really necessary because of the short computation time, it's great practice in using pickle.
+try:
+    pickle_file = open("lin_reg.pickle", "rb")
+except:
+    while acc < 0.95:
+        x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
+        linear = linear_model.LinearRegression()
+        linear.fit(x_train, y_train)
+        acc = linear.score(x_test, y_test)
+
+    with open("lin_reg.pickle", "wb") as file:
+        pickle.dump(linear, file)
+
+    pickle_file = open("lin_reg.pickle", "rb")
+
+linear = pickle.load(pickle_file)
+predictions = linear.predict(x_test)
+
+show_output(predictions, x_test, y_test, linear)
+
+plot_scatter_diagram(data)
+
